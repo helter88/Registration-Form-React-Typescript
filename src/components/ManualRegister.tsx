1395ea23414118161dbd267f19952ab3f8bed685
Artur <helter@wp.pl>
2022-03-29 18:55:13 +0200

    Implement email handler

--- src/components/ManualRegister.tsx
+++ src/components/ManualRegister.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import validator from 'validator';
 
 
 const ManualRegister = () => {
     
     const [emailWasClicked, setEmailWasClicked]= useState(false);
     const [passwordWasClicked, setPasswordWasClicked]= useState(false);
+    const [email, setEmail] = useState("");
 
     const emailBlurHandler= () =>{
         setEmailWasClicked(true);
@@ -14,6 +16,11 @@ const ManualRegister = () => {
         setPasswordWasClicked(true);
     }
 
+    const emailHandler = (e: React.ChangeEvent<HTMLInputElement> ) => {
+        console.log(e.target.value);
+        setEmail(e.target.value);
+    }
+
     let showEmialMessage = emailWasClicked ? <p className="text-sm text-rose-600">Wpisz swój email</p>:''
 
     let showPasswordMessage = passwordWasClicked ? <p className="text-sm text-rose-600">Wpisz swoje hasło i użyj minimum 6 znaków</p>: <p className="text-sm">Hasło powinno zawierać minimum 6 znaków</p>
@@ -21,7 +28,7 @@ const ManualRegister = () => {
     return(
         <div className="flex flex-col">
             <label htmlFor="email">Adres email</label>
-            <input type='text' placeholder="Wpisz swój adres email" id='email' onBlur={emailBlurHandler} className="bg-blue-100 max-w-xl p-1.5 border-2 my-3 rounded-lg" />
+            <input type='text' value= {email} placeholder="Wpisz swój adres email" id='email' onChange={emailHandler} onBlur={emailBlurHandler} className="bg-blue-100 max-w-xl p-1.5 border-2 my-3 rounded-lg" />
             {showEmialMessage}
             <label htmlFor="password">Hasło</label>
             <input type='text' placeholder="Wpisz swoje hasło" id='password' onBlur={passwordBlurHandler} className="bg-blue-100 max-w-xl p-1.5 border-2 my-3 rounded-lg" />
