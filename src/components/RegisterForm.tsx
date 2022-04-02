0881c13fca9c45767706d6ee587b4d35b48eab68
Artur <helter@wp.pl>
2022-04-02 11:21:55 +0200

    refactor SocialMediaButton

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,11 +1,16 @@
 import ManualRegister from "./ManualRegister";
 import SocialMediaButton from "./UI/SocialMediaButton";
 
+// const google = <svg className="icon-facebook">
+//     <use xlink
+    
+// </svg>
+
 const RegisterForm = () => {
     return(<div className="flex flex-col">
     <h3 className="p-1.5">Zarejestruj się</h3>
-    <SocialMediaButton logo='' text='Kontynuj z Google' />
-    <SocialMediaButton logo='' text='Kontynuj z Facebook' />
+    <SocialMediaButton logo='' text=''>Kontynuj z Google</SocialMediaButton>
+    <SocialMediaButton logo='' text=''>Kontynuj z Facebook</SocialMediaButton>
     <ManualRegister />
     </div>)
 }
--- src/components/UI/SocialMediaButton.tsx
+++ src/components/UI/SocialMediaButton.tsx
@@ -3,9 +3,9 @@ interface SocMedProps{
     logo: string;
 }
 
-const SocialMediaButton = (props: SocMedProps) => {
+const SocialMediaButton: React.FC<SocMedProps> = (props) => {
     return(
-        <button className="max-w-xl p-1.5 border-2 my-3 rounded-lg">{props.text}</button>
+        <button className="max-w-xl p-1.5 border-2 my-3 rounded-lg">{props.children}</button>
     )
 }
 export default SocialMediaButton;
