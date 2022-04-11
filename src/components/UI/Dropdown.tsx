830bed0526bdf02aafc4600b065bfd8e700e003e
Artur <helter@wp.pl>
2022-04-11 17:57:20 +0200

    add box-shadow to dropdown options box

--- src/components/UI/Dropdown.tsx
+++ src/components/UI/Dropdown.tsx
@@ -12,12 +12,12 @@ const Dropdown: React.FC<DropdownType>= (props) => {
       <div className='border rounded-lg p-2 py-3'>
           <img className= "h-4" src={polishFlag} alt='polishFlag' />
       </div>
-      <ul className='border rounded-md absolute w-48'>
+      <ul className='rounded-md absolute w-48 shadow-md'>
         <li>
-          <a className='flex ml-4'> <img className= "h-4" src={polishFlag} alt='polishFlag' /> Polski</a>
+          <a className='flex ml-4'> <img className= "h-4" src={polishFlag} alt='polishFlag' /> <p className ="ml-2 font-semibold">Polski</p></a>
           </li>
         <li>
-        <a className='flex ml-4'> <img className= "h-3.5" src={americanFlag} alt='americanFlag' /> English</a>
+        <a className='flex ml-4'> <img className= "h-3.5" src={americanFlag} alt='americanFlag' /> <p className ="ml-2 font-semibold">English</p></a>
           </li>
       </ul>
        
