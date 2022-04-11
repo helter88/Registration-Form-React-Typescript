e3038824c220b6473359d2dbb81fb3dd440686b1
Artur <helter@wp.pl>
2022-04-11 17:39:02 +0200

    add dropdown language options

--- src/components/UI/Dropdown.tsx
+++ src/components/UI/Dropdown.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import  polishFlag  from '../../icons/flags/poland.svg';
+import  americanFlag  from '../../icons/flags/american.svg';
 
 interface DropdownType {
   style: string
@@ -11,6 +12,14 @@ const Dropdown: React.FC<DropdownType>= (props) => {
       <div className='border rounded-lg p-2 py-3'>
           <img className= "h-4" src={polishFlag} alt='polishFlag' />
       </div>
+      <ul className='border rounded-md absolute w-48'>
+        <li>
+          <a className='flex ml-4'> <img className= "h-4" src={polishFlag} alt='polishFlag' /> Polski</a>
+          </li>
+        <li>
+        <a className='flex ml-4'> <img className= "h-3.5" src={americanFlag} alt='americanFlag' /> English</a>
+          </li>
+      </ul>
        
    </div>
   )
