e3fb54da42001d64a823d80877dee7fee5e73a89
Artur <helter@wp.pl>
2022-04-12 10:27:25 +0200

    add poppins font

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -8,7 +8,7 @@ import SocialMediaButton from "./UI/SocialMediaButton";
 
 const RegisterForm = () => {
     return(<div className="flex flex-col">
-    <h3 className="p-1.5">Zarejestruj się</h3>
+    <h3 className="p-1.5 text-xl font-bold">Zarejestruj się</h3>
     <SocialMediaButton logo='' text=''>Kontynuj z Google</SocialMediaButton>
     <SocialMediaButton logo='' text=''>Kontynuj z Facebook</SocialMediaButton>
     <SocialMediaButton logo='' text=''>Kontynuj z Apple</SocialMediaButton>
--- src/components/UI/Dropdown.tsx
+++ src/components/UI/Dropdown.tsx
@@ -11,10 +11,10 @@ const displayMenu = (fn1: ()=>void, fn2: ()=>void) => {
   return(
     <ul className='rounded-md absolute w-48 shadow-md py-2' onMouseEnter={fn1} onMouseLeave={fn2}>
 <li className='hover:bg-slate-100 py-2 text-slate-500 hover:text-slate-900 cursor-pointer'>
-  <a className='flex ml-4'> <img className= "h-4" src={polishFlag} alt='polishFlag' /> <p className ="ml-2 font-semibold">Polski</p></a>
+  <a className='flex ml-4'> <img className= "h-4" src={polishFlag} alt='polishFlag' /> <p className ="ml-2 text-sm font-semibold">Polski</p></a>
   </li>
 <li  className='hover:bg-slate-100 py-2 text-slate-500 hover:text-slate-900 cursor-pointer'>
-<a className='flex ml-4'> <img className= "h-3.5" src={americanFlag} alt='americanFlag' /> <p className ="ml-2 font-semibold -translate-y-1">English</p></a>
+<a className='flex ml-4'> <img className= "h-3.5" src={americanFlag} alt='americanFlag' /> <p className ="ml-2 text-sm font-semibold -translate-y-1">English</p></a>
   </li>
 </ul>
   )
